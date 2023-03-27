1aeae486b984a6a7950f24210851a21782247340
devholiday <devholidaydev@gmail.com>
2023-03-28 00:50:23 +0300

    update

--- components/product-view-card.js
+++ components/product-view-card.js
@@ -13,27 +13,38 @@ export default function ProductViewCard({product, disabledBuy=false}) {
             <Link href={'/product/' + product.id} className={styles.link}></Link>
 
             <div className={styles.wrapper}>
-                <div className={styles.wrapperImg}>
-                    {product.image ? <img src={product.image} /> : <PlaceholderSVG />}
-                </div>
-                <div className={styles.info}>
-                    <div className={styles.priceBlock}>
-                        {product.compareAtPrice ? (
-                            <>
-                                <span className={styles.compareAtPrice}>&#8362;{product.price.toFixed(2)}</span>
-                                <span className={styles.oldPriceWithLine}>
-                                    <span className={styles.oldPrice}>&#8362;{product.compareAtPrice.toFixed(2)}</span>
-                                    <span className={styles.line}></span>
-                                </span>
-                            </>
-                        ) : <span className={styles.price}>&#8362;{product.price.toFixed(2)}</span>}
+                <div className={styles.container}>
+                    <div className={styles.containerImg}>
+                        <div className={styles.wrapperImg}>
+                            <div className={styles.postionImage}>
+                                <div className={styles.image}>
+                                    {product.image ? <img src={product.image} /> : <PlaceholderSVG />}
+                                </div>
+                            </div>
+                        </div>
                     </div>
-
-                    <h3 className={styles.title}>{product.title[locale]}</h3>
-                    <span className={styles.weightInfo}>{product.displayAmount} {product.unit}</span>
+                    <div className={styles.containerInfo}>
+                        <div className={styles.priceTitle}>
+                            <div className={styles.priceBlock}>
+                                {product.compareAtPrice ? (
+                                    <>
+                                        <span className={styles.compareAtPrice}>&#8362;{product.price.toFixed(2)}</span>
+                                        <span className={styles.oldPriceWithLine}>
+                                            <span className={styles.oldPrice}>&#8362;{product.compareAtPrice.toFixed(2)}</span>
+                                            <span className={styles.line}></span>
+                                        </span>
+                                    </>
+                                ) : <span className={styles.price}>&#8362;{product.price.toFixed(2)}</span>}
+                            </div>
+                            <h3 className={styles.title}>{product.title[locale]}</h3>
+                            <span className={styles.weightInfo}>{product.displayAmount} {product.unit}</span>
+                        </div>
+                        <div>
+                            {!disabledBuy && <BuyButton disabled={!product.availableForSale} 
+                            productId={product.id} price={product.price} secondary/>}
+                        </div>
+                    </div>
                 </div>
-                {!disabledBuy && <BuyButton disabled={!product.availableForSale} 
-                productId={product.id} price={product.price} secondary/>}
             </div>
         </div>
     );
